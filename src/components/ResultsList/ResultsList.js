e2f33c766c5b896e242789afc286001cc108418c
Radu Handolescu <rhandole@uci.edu>
2017-10-16 22:39:27 -0700

    new file:   src/components/ListItem/ListItem.css 	new file:   src/components/ListItem/ListItem.js 	new file:   src/components/ResultsList/ResultsList.css 	new file:   src/components/ResultsList/ResultsList.js 	modified:   src/components/Search/SearchBar.js 	modified:   src/index.css 	modified:   src/index.js 	new file:   src/index.js.orig

---
+++ src/components/ResultsList/ResultsList.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import './ResultsList.css'
+import Arrow from '../../assets/arrow.png' //.. moves 1 folder above
+import LoadingGif from '../../assets/giphy.gif'
+import PropTypes from 'prop-types'
+import ListItem from '../ListItem/ListItem'
+
+const ResultsList = (props) => {
+    if (props.loading){
+        return (
+            <div className="results-list-container">
+                <img src = {LoadingGif} className="loading-gif" />
+            </div>
+        )
+    } else if (props.results.length > 0){
+        return(
+            <div className="results-list-container">
+                <div className="results-list">
+                    {props.results.map((item, i)=>{
+                        if (item.results.toUpperCase() !== 'out of bussiness'.toUpperCase()){
+                            return (
+                                <ListItem key={i} restaurant={item} />
+                            )
+                        }
+                    })}
+                </div>
+            </div>
+        )
+    } else if (props.searched){
+        return(
+            <div className="results-list-container">
+                <div className="no-results-found">
+                    <h2>Sorry, we couldn't find restaurants with that name</h2>
+                </div>
+            </div>
+        )
+    } 
+    else {
+        return (
+            <div className="results-list-container">
+                <h2>Kick it off by searching up top!</h2>
+                <img src = {Arrow} className="arrow" />
+            </div>
+        )
+    }
+}
+
+ResultsList.propTypes = {
+    loading: PropTypes.bool.isRequired,
+    searched: PropTypes.bool.isRequired
+}
+
+export default ResultsList
